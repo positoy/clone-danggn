c03f0d3eb00736631833293687836c3d44df3b86
positoy <positoy@gmail.com>
2020-07-13 00:43:00 +0900

    initial commit

---
+++ src/main/java/navercorp/com/andy/AndyApplication.java
@@ -0,0 +1,13 @@
+package navercorp.com.andy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndyApplication.class, args);
+	}
+
+}
