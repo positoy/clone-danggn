dd9ed1a6b592f82eadd907298829da60b8f76fd7
positoy <positoy@gmail.com>
2020-07-13 23:31:22 +0900

    update home view

---
+++ src/main/resources/static/css/data.js
@@ -0,0 +1,140 @@
+var dump = [
+    {
+        title : "라오스 다이어리",
+        img : "img/001_1.jpg",
+        area : "신동",
+        timestamp : 1594645127700,
+        price : 2000,
+        chat:0,
+        liked:5
+    },
+    {
+        title : "자이글",
+        img : "img/002_1.jpg",
+        area : "권선동",
+        timestamp : 1594645127642,
+        price : 20000,
+        chat:0,
+        liked:0
+    },
+    {
+        title : "라오스 다이어리",
+        img : "img/001_1.jpg",
+        area : "신동",
+        timestamp : 1594645127700,
+        price : 2000,
+        chat:0,
+        liked:5
+    },
+    {
+        title : "삼성 파워건 VS80M8090KC 싸게 팔아요",
+        img : "img/003_1.jpg",
+        area : "수원시 팔달구 인계동",
+        timestamp : 1594645127622,
+        price : 270000,
+        chat:1,
+        liked:3
+    },
+    {
+        title : "삼성 파워건 무선청소기 새 배터리!",
+        img : "img/004_1.jpg",
+        area : "수원시 팔달구 인계동",
+        timestamp : 1594645127615,
+        price : 150000,
+        chat:0,
+        liked:3
+    }
+];
+
+var dump2 = [
+    {
+        user : {
+            id : "EBSHiN",
+            area : "신동",
+            user_img : "img/004_1.jpg",
+            manner : 42.8
+        },
+        goods : {
+            title : "삼성 파워건 무선청소기 새 배터리!",
+            category : "디지털/가전",
+            body : "삼성 무선청소기 새 배터리 판매합니다~ 센터에서 20만원에 판매되고 있습니다. 구매 영수증 원하시면 확인해드릴 수 있습니다 ^^ 올 5월 말일날 구매했습니다~",
+            goods_imgs : ["img/004_1.jpg", "img/004_2.jpg"],
+            timestamp : 1594645127615,
+            price : 150000,
+            chat:0,
+            liked:3
+        }
+    },
+    {
+        user : {
+            id : "porta",
+            area : "수원시 팔달구 인계동",
+            user_img : "img/004_1.jpg",
+            manner : 53.2
+        },
+        goods : {
+            title : "삼성 파워건 무선청소기 새 배터리!",
+            category : "디지털/가전",
+            body : "삼성 무선청소기 새 배터리 판매합니다~ 센터에서 20만원에 판매되고 있습니다. 구매 영수증 원하시면 확인해드릴 수 있습니다 ^^ 올 5월 말일날 구매했습니다~",
+            goods_imgs : ["img/004_1.jpg", "img/004_2.jpg"],
+            timestamp : 1594645127615,
+            price : 150000,
+            chat:0,
+            liked:3
+        }
+    },
+    {
+        user : {
+            id : "porta",
+            area : "수원시 팔달구 인계동",
+            user_img : "img/004_1.jpg",
+            manner : 53.2
+        },
+        goods : {
+            title : "삼성 파워건 무선청소기 새 배터리!",
+            category : "디지털/가전",
+            body : "삼성 무선청소기 새 배터리 판매합니다~ 센터에서 20만원에 판매되고 있습니다. 구매 영수증 원하시면 확인해드릴 수 있습니다 ^^ 올 5월 말일날 구매했습니다~",
+            goods_imgs : ["img/004_1.jpg", "img/004_2.jpg"],
+            timestamp : 1594645127615,
+            price : 150000,
+            chat:0,
+            liked:3
+        }
+    },
+    {
+        user : {
+            id : "porta",
+            area : "수원시 팔달구 인계동",
+            user_img : "img/004_1.jpg",
+            manner : 53.2
+        },
+        goods : {
+            title : "삼성 파워건 무선청소기 새 배터리!",
+            category : "디지털/가전",
+            body : "삼성 무선청소기 새 배터리 판매합니다~ 센터에서 20만원에 판매되고 있습니다. 구매 영수증 원하시면 확인해드릴 수 있습니다 ^^ 올 5월 말일날 구매했습니다~",
+            goods_imgs : ["img/004_1.jpg", "img/004_2.jpg"],
+            timestamp : 1594645127615,
+            price : 150000,
+            chat:0,
+            liked:3
+        }
+    },
+    {
+        user : {
+            id : "porta",
+            area : "수원시 팔달구 인계동",
+            user_img : "img/004_1.jpg",
+            manner : 53.2
+        },
+        goods : {
+            title : "삼성 파워건 무선청소기 새 배터리!",
+            category : "디지털/가전",
+            body : "삼성 무선청소기 새 배터리 판매합니다~ 센터에서 20만원에 판매되고 있습니다. 구매 영수증 원하시면 확인해드릴 수 있습니다 ^^ 올 5월 말일날 구매했습니다~",
+            goods_imgs : ["img/004_1.jpg", "img/004_2.jpg"],
+            timestamp : 1594645127615,
+            price : 150000,
+            chat:0,
+            liked:3
+        }
+    },
+];
