dd772f268fa16cd0160d9e551395b3606ea66357
positoy <positoy@gmail.com>
2020-07-18 15:07:27 +0900

    (html) add submit condition in write.html

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -3,7 +3,33 @@ window.onload = function() {
 
     $("#write_complete_button").click (function(){
         console.log("#goods_form submitted.");
-        $("#goods_form").submit();
+
+        var $form = $("#write_form");
+        var $title = $("#write_title");
+        var $category = $("#write_category option:selected");
+        var $price = $("#write_price");
+        var $body = $("#write_body");
+
+        do {
+            console.log($category.val());
+            if ($category.val() == "카테고리") {
+                alert("카테고리를 설정해주세요");
+                break;
+            }
+            if ($title.val().length == 0) {
+                alert("제목을 입력해주세요");
+                break;
+            }
+            if ($body.val().length == 0) {
+                alert("내용을 입력해주세요");
+                break;
+            }
+            if ($price.val().length == 0)
+                $price.val(0);
+
+            $form.submit();
+        } while (false);
+
     });
 
     $("#back-button").click (function(){
