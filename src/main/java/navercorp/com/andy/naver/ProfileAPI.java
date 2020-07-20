af84811ea8740801bb7ce12da093bb92db40d713
positoy <positoy@gmail.com>
2020-07-20 13:22:15 +0900

    (java) fix a bug regarding wrong Profile Initialization param

--- src/main/java/navercorp/com/andy/naver/ProfileAPI.java
+++ src/main/java/navercorp/com/andy/naver/ProfileAPI.java
@@ -28,6 +28,6 @@ public class ProfileAPI {
         String profile_image = (String) ((JSONObject)json.get("response")).get("profile_image");
         String name = (String) ((JSONObject)json.get("response")).get("name");
 
-        return (id == null) ? null : new Profile(id, nickname, profile_image, name);
+        return (id == null) ? null : new Profile(id, name, nickname, profile_image);
     }
 }
