ba513b223d5af79b5d21d6973b8de3dfd456601e
positoy <positoy@gmail.com>
2020-07-20 11:38:21 +0900

    (java) fix session creation hangout problem regarding securerandom

--- src/main/java/navercorp/com/andy/AndyApplication.java
+++ src/main/java/navercorp/com/andy/AndyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AndyApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.security.egd", "file:/dev/./urandom");
 		SpringApplication.run(AndyApplication.class, args);
 	}
 
